aaef7b3242ee1b471ca3765a425dd33ee1f14b7c
Yann Davin <yanndvn@gmail.com>
2020-11-25 22:57:41 +0100

    [FEAT] Mentions légales, alignement du texte

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
-window.addEventListener("click", () => {
-    document.getElementById("player").play();
-    document.getElementById("chromepls").classList.add("itsokay");
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("chromepls").addEventListener("click", () => {
+        document.getElementById("player").play();
+        document.getElementById("chromepls").classList.add("itsokay");
+    });
+
+    const stopEvent = (evt) => {
+        evt.preventDefault();
+        evt.stopPropagation();
+    }
+
+    const openDialog = (evt) => {
+        document.getElementById("credits").classList.remove("hidden");
+        stopEvent(evt);
+    };
+    const closeDialog = (evt) => {
+        if (["mentions", "modal"].includes(evt.target.id)) {
+            return;
+        }
+        document.getElementById("credits").classList.add("hidden");
+        stopEvent(evt);
+    };
+    document.getElementById("mentions").addEventListener("click", openDialog);
+    document.addEventListener("click", closeDialog);
 });
